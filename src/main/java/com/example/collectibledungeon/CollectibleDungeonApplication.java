823dc3aed2a85da2953db16a4a1ad22925f363b7
Nicolas <gabriel.soliz@alumnos.frm.utn.edu.ar>
2021-10-19 12:36:23 -0300

    Vista index

--- src/main/java/com/example/collectibledungeon/CollectibleDungeonApplication.java
+++ src/main/java/com/example/collectibledungeon/CollectibleDungeonApplication.java
@@ -1,18 +1,12 @@
 package com.example.collectibledungeon;
 
-import com.example.collectibledungeon.entities.Collectible;
-import com.example.collectibledungeon.entities.License;
-import com.example.collectibledungeon.entities.Producer;
-import com.example.collectibledungeon.services.CollectibleService;
-import com.example.collectibledungeon.services.LicenseService;
-import com.example.collectibledungeon.services.ProducerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CollectibleDungeonApplication {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(CollectibleDungeonApplication.class, args);
     }
 }
